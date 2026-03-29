8f9f5e0652890811e940af97afe285da6e620fc1
akitorahayashi <hayashi20030703@gmail.com>
2026-03-29 15:53:48 +0900

    feat: Add development aliases for 'mise' and 'go' commands; remove obsolete 'go' aliases

---
+++ src/assets/ansible/roles/shell/config/global/alias/dev/mise.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+dev_alias_as "mise" "ms" "run"
+
+alias ms-tr='mise trust'
+
+alias ms-p='mise prepare'
+
+alias ms-ls='mise ls'
+alias ms-ls-r='mise ls-remote'
+
+alias ms-i='mise install'
+alias ms-i-l='mise install --locked'
